e4498cbaa6161235a1c7ff7dc5c859c0c97c1d09
Le Mortellec Loic <lemortellec.loic@gmail.com>
2019-10-25 14:52:15 +0200

    ajout fonction charToInt

--- main.c
+++ main.c
@@ -19,8 +19,8 @@ int chainSize(const char str[]){
 }
 /*
 description : allows to have the opposite of the all number in the array in the same array
-parameter   : int size : to define size of Array
-            : int str[]: point on the Array to work
+parameter   : int str[]: point on the Array to work
+            : int size : to define size of Array
 return      : return opposite of all the numbers in the array in the same array
 */
 void digitInverse(char str[], int size){
@@ -33,12 +33,40 @@ void digitInverse(char str[], int size){
             y--;
     }
 }
+/*
+description : allows transforms number or letter char in number int with ASCII array
+parameter   : char c : char to transforms
+return      : the equivalent digit
+*/
+int charToInt(char c){
+    if(c>47 && c<57){
+        return c-= 48;
+    }
+    else if(c>64 && c<71){
+        return c-= 55;
+    }
+    else if(c>96 && c<103){
+        return c-= 87;
+    }
+}
+/*
+description :
+parameter   :
+return      :
+*/
+void convertDicimalToBinary(int number, char bin[]){
 
+
+}
 
 int main()
 {
-    char test[] = "12345";
-    digitInverse(test, chainSize(test));
-    printf("%s\n", test);
+    char test[] = "1f3A55b";
+    //digitInverse(test, chainSize(test));
+    for(int i=0;i<chainSize(test);i++){
+
+        printf("%d   ", charToInt(test[i]));
+    }
+
     return 0;
 }
