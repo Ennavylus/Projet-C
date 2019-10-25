0cacb0d354ac74ca388d73eef59f8ffe93dd68e3
Le Mortellec Loic <lemortellec.loic@gmail.com>
2019-10-25 17:37:40 +0200

    modification fonction convertHaxaToDecimal

--- main.c
+++ main.c
@@ -42,10 +42,10 @@ void digitInverse(char str[], int size){
     int y = size-1;
     int t;
     for(int i = 0; i<y; i++){
-            int t = str[i];
-            str[i] = str[y];
-            str[y] = t;
-            y--;
+        int t = str[i];
+        str[i] = str[y];
+        str[y] = t;
+        y--;
     }
 }
 /*
@@ -95,55 +95,58 @@ void addFisrt(char str[], char Add){
 /*
 description : allows to convert decimal into binary  in a new string
 parameter   : int number : number to converse
+            : char bin[] : new string to recive convert
 return      :
 */
 void convertDecimalToBinary(int number, char bin[]){
-      int r =number;
-        while(r>0){
+    int r =number;
+    while(r>0){
         int t = r%2;
         r /=2;
         addFisrt(bin, intToChar(t));
- }
+    }
 }
 /*
 description : allows to convert decimal into octal  in a new string
 parameter   : int number : number to converse
+            : char octa[] : new string to recive convert
 return      :
 */
 void convertDecimalToOctal(int number, char octal[]){
-      int r =number;
-        while(r>0){
+    int r =number;
+    while(r>0){
         int t = r%8;
         r /=8;
         addFisrt(octal, intToChar(t));
- }
+    }
 }
 /*
 description : allows to convert decimal into hexadecimal  in a new string
 parameter   : int number : number to converse
+            : char hexa[] : new string to recive convert
 return      :
 */
 void convertDecimalToHexa(int number, char hexa[]){
-      int r =number;
-        while(r>0){
+    int r =number;
+    while(r>0){
         int t = r%16;
         r /=16;
         addFisrt(hexa, intToChar(t));
- }
+    }
 }
-
+/*
+description : allows to convert Hexadecimal into Decimal  in a new string
+parameter   : char hexa[] : string hexa to convert
+return      : retruns decimal number in base 10 fo hexadecimal string
+*/
 int convertHexaToDecimal(char hexa[]){
     digitInverse(hexa,chainSize(hexa));
-      int nb= 0;
-      int nbsomme = 0;
-        for(int i = 0; i<chainSize(hexa);i++){
-
-            nbsomme +=power(charToInt(hexa[i]),nb);
-            printf("%d\n",power(charToInt(hexa[i]),nb));
-        printf("%d\n",nbsomme);
-            nb++;
+    int nbsomme = 0;
+    for(int i = 0; i<chainSize(hexa);i++){
+        nbsomme += charToInt(hexa[i])*(int)pow(16,i);
     }
-return nbsomme;
+    digitInverse(hexa,chainSize(hexa));
+    return nbsomme;
 
 }
 
@@ -152,15 +155,18 @@ return nbsomme;
 int main()
 {
     char test[] = "DEF";
-    int test1= 19358;
+    char test2[]= "";
     //digitInverse(test, chainSize(test));
    // printf(" %d est egale a %c", test1, intToChar(test1));
-   //convertDicimalToBinary(test1,test);
-    printf("%d\n",convertHexaToDecimal(test));
+   convertDecimalToHexa(convertHexaToDecimal(test),test2);
+  //  printf("%d\n",convertHexaToDecimal(test));
+
 //    for(int i=0;i<chainSize(test);i++){
 //
 //        printf("%d   ", charToInt(test[i]));
 //    }
+   // printf("%s\n",test );
+    printf("%s\n", test2);
 
     return 0;
 }
